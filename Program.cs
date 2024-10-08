5beec8fc47abea22652a61e5af41e5c7482e69a2
samettas52 <166943077+samettas52@users.noreply.github.com>
2024-10-08 14:11:09 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            double piyasaBugun = 115000.23;
+            double piyasaDun = 116000;
+            string mesajArtis = "Artış oku";
+            string mesajDüşüş = "Düşüş oku";
+            string mesajSabit = "Sabit oku";
+            //EĞER TRUE FALSE İLE ÇÖZEBİLCEĞİMZ BİR KODSA
+            //bool sonuc= piyasaBugun < piyasaDun;
+            //if (sonuc == true)
+            //{
+            //    Console.WriteLine(mesajArtis);
+            //}
+            //else 
+            //{
+            //    Console.WriteLine(mesajDüşüş);
+            //}
+
+            //EĞER TRUE FASLE DEĞERLERİNDEN FAZLA BİR DEĞER VARSA
+            if (piyasaBugun > piyasaDun)
+            {
+                Console.WriteLine(mesajArtis);
+            }
+            else if (piyasaBugun == piyasaDun)
+            {
+                Console.WriteLine(mesajSabit);
+            }
+            else
+            {
+                Console.WriteLine(mesajDüşüş);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("ders2.1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ders2.1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("c897cc3e-6746-4896-bf9f-5ce25bf73d8b")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
